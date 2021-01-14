d376b8827c9af581658af2bf708daf66c0969c22
Luis Canahuate <luis.canahuate16@gmail.com>
2021-01-14 14:55:09 -0500

    hotfix

--- Develop/server.js
+++ Develop/server.js
@@ -15,20 +15,20 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect("mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://canahuate16:Etauhanac16@cluster0.ak7rf.mongodb.net/budget?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb+srv://canahuate16:Etauhanac16@cluster0.ak7rf.mongodb.net/budget?retryWrites=true&w=majority',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+//   }
+// );
 
 // routes
 app.use(require("./routes/api.js"));
